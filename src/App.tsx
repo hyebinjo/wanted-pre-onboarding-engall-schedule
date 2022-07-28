52431f418b981639486fd3714966e2f3cf85dfef
hyebinjo <hyebinjo91@gmail.com>
2022-07-29 01:21:56 +0900

    style: Create GlobalStyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { GlobalStyle } from './styles/GlobalStyle';
 import Add from './pages/Add';
 import Schedule from './pages/Schedule';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Schedule />} />
-        <Route path="/add" element={<Add />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Schedule />} />
+          <Route path="/add" element={<Add />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 

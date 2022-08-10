8172e99ef4e482af8a6e8369b1da2aceeddc1436
hyebinjo <hyebinjo91@gmail.com>
2022-08-10 21:30:06 +0900

    chore: 라우터 구조 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,18 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { GlobalStyle } from './styles/GlobalStyle';
 import Add from './pages/Add';
 import Schedule from './pages/Schedule';
-import Header from './components/Header';
+import Layout from './Layout';
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <Header />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Schedule />} />
-          <Route path="/add" element={<Add />} />
+          <Route path="" element={<Layout />}>
+            <Route index element={<Schedule />} />
+            <Route path="/add" element={<Add />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>

9d57636d42a1cd3acd73cbef310b9c65ab87aba6
hyebinjo <hyebinjo91@gmail.com>
2022-08-10 21:30:30 +0900

    feat: 로고 링크 연결

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,29 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 function Header() {
-  return <H1>engall</H1>;
+  return (
+    <HeaderBar>
+      <Link to="">
+        <H1>engall</H1>
+      </Link>
+    </HeaderBar>
+  );
 }
 
 export default Header;
 
-const H1 = styled.h1`
+const HeaderBar = styled.h1`
+  display: flex;
+  align-items: center;
   height: 4rem;
   width: 100vw;
   background-color: #44a7c8;
+  padding: 0 1rem;
+`;
+
+const H1 = styled.h1`
   color: white;
   font-size: 2rem;
-  line-height: 4rem;
-  padding: 0 1rem;
+  cursor: pointer;
 `;

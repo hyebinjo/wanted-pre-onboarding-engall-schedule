791607266560fb4fb848914221a958662697dc46
hyebinjo <hyebinjo91@gmail.com>
2022-08-10 14:57:51 +0900

    chore: 타입 추가

--- src/interfaces/types.ts
+++ src/interfaces/types.ts
@@ -1,1 +1,11 @@
 export type TimeRange = { start: string; end: string };
+
+export type Days = 'mon' | 'tue' | 'wed' | 'thu' | 'fri' | 'sat' | 'sun';
+
+export type jsonLecture = {
+  id: number;
+  day: Days;
+  startTime: string;
+};
+
+export type jsonScheduleType = jsonLecture[];
